bf2d2d7bf37ea48b0463907e72d0a7bd47beb38c
Cassandra <cassandramarcussencello@gmail.com>
2023-03-01 17:19:50 -0500

    Adding dependencies and messageList to MessageCore

--- src/main/java/ScaleModel/MessageCore.java
+++ src/main/java/ScaleModel/MessageCore.java
@@ -1,8 +1,10 @@
 package ScaleModel;
 
 import ScaleModel.grpc.MessageRequest;
+import ScaleModel.objects.Event;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class MessageCore {
@@ -10,8 +12,12 @@ public class MessageCore {
     // TODO: this doesn't work
     private final List<Event> eventList;
 
+    /**
+     * Default MessageCore constructor instantiates a new messageQueue.
+     */
     public MessageCore() {
         messageQueue = new LinkedList<>();
+        eventList = new LinkedList<>();
     }
 
     public void queueMessage(MessageRequest message) {
