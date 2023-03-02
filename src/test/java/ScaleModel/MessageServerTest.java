f4d7fe6eb3f7424231bc3bd4f4aade2433fa3a73
Cassandra <cassandramarcussencello@gmail.com>
2023-03-02 15:09:26 -0500

    Adding comments to test file

--- src/test/java/ScaleModel/MessageServerTest.java
+++ src/test/java/ScaleModel/MessageServerTest.java
@@ -5,13 +5,18 @@ import org.junit.jupiter.api.Test;
 import ScaleModel.grpc.MessageRequest;
 
 /**
- * Tests (to be continued)
+ * Unit tests for the MessageServer class. These unit tests encompass the various functionalities of 
+ * MessageServer, including sending one or multiple messages, receiving a message, and performing an internal event.
+ * These tests mostly concern how the logical clock gets updated.
  */
 class MessageServerTest {
 
     private static final String testTargetOne = "testTargetOne";
     private static final String testTargetTwo = "testTargetTwo";
 
+    /**
+     * Test that core's initial logical clock time is zero.
+     */
     @Test 
     void testInitialTimeZero() {
         MessageCore core = new MessageCore();
@@ -19,6 +24,9 @@ class MessageServerTest {
         Assertions.assertTrue(core.getTime() == 0);
     }
 
+    /**
+     * Testing that the logical clock time was updated by 1 when one message was sent
+     */
     @Test
     void testSendOneMessage() {
         MessageCore core = new MessageCore();
@@ -26,10 +34,12 @@ class MessageServerTest {
         MessageServer.sendOneMessage(testTargetOne, core);
         int updatedTime = core.getTime();
 
-        // Testing that the logical clock time was updated by 1.
         Assertions.assertTrue(updatedTime == currentTime + 1);
     }
 
+     /**
+     * Testing that the logical clock time was updated by 1 when two messages were sent
+     */
     @Test
     void testSendTwoMessages() {
         MessageCore core = new MessageCore();
@@ -37,10 +47,12 @@ class MessageServerTest {
         MessageServer.sendTwoMessages(testTargetOne, testTargetTwo, core);
         int updatedTime = core.getTime();
 
-        // Testing that the logical clock time was updated by 1.
         Assertions.assertTrue(updatedTime == currentTime + 1);
     }
 
+     /**
+     * Testing that the logical clock time was updated by 1 when an internal event took place
+     */
     @Test
     void testInternalEvent() {
         MessageCore core = new MessageCore();
@@ -48,15 +60,17 @@ class MessageServerTest {
         MessageServer.internalEvent(core);
         int updatedTime = core.getTime();
 
-        // Testing that the logical clock time was updated by 1.
         Assertions.assertTrue(updatedTime == currentTime + 1);
     }
 
+     /**
+     * Testing that the logical clock time updates to the max of the local logical clock time 
+     * and request's logical clock time, plus one.
+     */
     @Test
     void testReceiveMessage() {
         MessageCore core = new MessageCore();
-        // Large test time, for testing that the logical clock time updates to the max of the local logical clock time 
-        // and request's logical clock time, plus one.
+        // Large test time
         int large_test_time = 1000;
         MessageRequest request = MessageRequest.newBuilder().setLogicalTime(large_test_time).build();
 
